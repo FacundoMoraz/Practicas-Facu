8a1d88cddb0d1293647ae6add93354d43932fdb5
FacundoMoraz <swowrdd0@gmail.com>
2024-04-30 20:24:25 -0300

    actividades 7 a 8

---
+++ ejer7.js
@@ -0,0 +1,7 @@
+const num1=1;
+const num2=10;
+for(let i=num1; i<=num2;i++) {
+    if(i%3===0){
+        console.log(i);
+    }
+}
---
+++ ejercicio8.js
@@ -0,0 +1,17 @@
+//Mostrar los multiplos de 2 y 5 menores a 100.
+let i=1
+let o=1
+console.log("multiplos de 2");
+while (i<100){
+    if (i%2 === 0){
+        console.log(i)
+    }
+    i++;
+}
+console.log("multiplos de 5");
+while (o<100){
+    if (o%5 === 0){
+        console.log(o)
+    }
+    o++;
+}
